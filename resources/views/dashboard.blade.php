bb7d31a95063d3a4b835ffa9f89ece8e2bfbf468
NazikD <nazar.doroshenkov@gmail.com>
2023-03-08 10:58:35 +0200

    Connect Bootstrap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,27 +1,45 @@
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Donator Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Message</th>
-            <th scope="col">Date</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($posts as $post)
-        <tr>
-            <td>{{$post->Name}}</td>
-            <td>{{$post->Email}}</td>
-            <td>{{$post->Amount}}</td>
-            <td>{{$post->Message}}</td>
-            <td>{{$post->created_at->format('Y-m-d')}}</td>
-        </tr>
-        @endforeach
-    </tbody>
+<!DOCTYPE html>
+<html lang="en">
 
-</table>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-<div>
-    {{$posts->links()}}
-</div>
+<body>
+
+    <div class="table__container" style="padding: 100px;">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Donator Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Message</th>
+                    <th scope="col">Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($posts as $post)
+                <tr>
+                    <td>{{$post->Name}}</td>
+                    <td>{{$post->Email}}</td>
+                    <td>{{$post->Amount}}</td>
+                    <td>{{$post->Message}}</td>
+                    <td>{{$post->created_at->format('Y-m-d')}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+        <div>
+            {{$posts->links()}}
+        </div>
+    </div>
+
+</body>
+
+</html>
