1e4c608a4af5d11fe9b008071c2197bbe1b246e6
NazikD <nazar.doroshenkov@gmail.com>
2023-03-09 14:11:45 +0200

    create branch

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,40 +1,37 @@
 @extends ('layouts.main')
 @section('content')
-<div>
-    <table class="table">
-        <thead>
-            <tr style="width: auto">
-                <th scope="col">Donator Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Amount</th>
-                <th scope="col" style="max-width: 400px">Message</th>
-                <th scope="col">Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($posts as $post)
-            <tr>
-                <td>{{ $post->Name }}</td>
-                <td>{{ $post->Email }}</td>
-                <td>{{ $post->Amount }}</td>
-                <td style="max-width: 400px">{{ $post->Message }}</td>
-                <td>{{ $post->created_at->format('Y-m-d') }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-
-    </table>
     <div>
-        {{ $posts->links() }}
+        <table class="table">
+            <thead>
+                <tr style="width: auto">
+                    <th scope="col">Donator Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col" style="max-width: 400px">Message</th>
+                    <th scope="col">Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td>{{ $post->Name }}</td>
+                        <td>{{ $post->Email }}</td>
+                        <td>{{ $post->Amount }}</td>
+                        <td style="max-width: 400px">{{ $post->Message }}</td>
+                        <td>{{ $post->created_at->format('Y-m-d') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+        <div>
+            {{ $posts->links() }}
+        </div>
     </div>
-</div>
 @endsection
 
 @section('button-create')
-
-
-<div style="float: right;">
-    <a href="{{route('post.create')}}" class="btn btn-primary">Add one</a>
-</div>
-
+    <div style="float: right;">
+        <a href="{{ route('post.create') }}" class="btn btn-primary">Add one</a>
+    </div>
 @endsection
